abe540790e989f06211fee8a6ad67f0066bda79e
Michael Kirss <Michaelkirss@gmail.com>
2023-05-04 20:10:24 +0400

    class Pawn test passed add commit

--- src/Pawn.java
+++ src/Pawn.java
@@ -8,23 +8,47 @@ public class Pawn extends ChessPiece{
     }
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn){
-        int [][] rules = new int[][] {{1,0},{2,0}};
-        int posLine;
-        int posColumn;
+        int [][] rules = new int[][] {{1,0},{2,0},{-1,0},{-2,0}};
+        int posLine = 0;
+        int posColumn = 0;
         int board = 7;
-        posLine = line + rules[0][0];
-        posColumn = column + rules[0][1];
-            if (posLine < 0 || posColumn < 0 ||
-                    posLine > board || posColumn > board) {
+        if (line == toLine) {
+            return false;
+        }
+        if (getColor()=="White") {
+            if (line == 1) {
+                posLine = line + rules[1][0];
+                if (toLine == posLine) {
+                    return true;
+                }
+            }
+            posLine = line + rules[0][0];
+            posColumn = column;
+            if (posLine > board) {
                 return false;
             }
-            if (toLine == posLine && toColumn == posColumn) {
-                return true;
+        } else if (getColor()=="Black") {
+            if (line == 6) {
+                posLine = line + rules[3][0];
+                if (toLine == posLine) {
+                    return true;
+                }
             }
+            posLine = line + rules[2][0];
+            posColumn = column;
+            if (posLine < 0 ) {
                 return false;
+            }
+                }
+
+        if (toLine == posLine  && toColumn == posColumn) {
+            return true;
+        }
+        return false;
     }
     @Override
     public String getSymbol(){
         return "P";
     }
+
 }
