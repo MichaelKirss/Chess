699447f243a2b3bf136070401e45a19ceda78e1c
Michael Kirss <Michaelkirss@gmail.com>
2023-05-04 20:43:47 +0400

    class Bishop test passed add commit

---
+++ src/Bishop.java
@@ -0,0 +1,30 @@
+public class Bishop extends ChessPiece {
+    public Bishop(String color){
+        super (color);
+    }
+    @Override
+    public String getColor(){
+        return color;
+    }
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn){
+        int board = 7;
+        int decLine = Math.abs(toLine - line);
+        int decColumn = Math.abs(toColumn - column);
+        if (toLine == line && toColumn == column){
+            return false;
+        }
+        if (toLine > board || toColumn > board ||
+            toLine < 0     || toColumn < 0){
+            return false;
+        }
+        if (decLine == decColumn){
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public String getSymbol(){
+        return "B";
+    }
+}
